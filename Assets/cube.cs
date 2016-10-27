2e062dff50ce11a21ae8b46406d1846b056054f1
ec965 <enoch965@gmail.com>
2016-10-27 16:51:22 -0700

    cube movement

---
+++ Assets/cube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+    public float move_speed;
+	// Use this for initialization
+	void Start () {
+        move_speed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.Translate(move_speed*Input.GetAxis("Horizontal")* Time.deltaTime, 0f, move_speed*Input.GetAxis("Vertical") * Time.deltaTime);
+        
+    }
+}
