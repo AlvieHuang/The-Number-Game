f10238a9e7cc0636c69e15eb38f95a3868a3ffa9
AlvieHuang <Alvinh7@uci.edu>
2016-11-03 16:48:16 -0700

    !st Person & Room
    Room has most models from free Asset Store

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position;
+	}
+}
---
+++ Assets/Player/PlayerMovements.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovements : MonoBehaviour {
+
+    public float move_speed;
+    public float rotate_speed;
+
+    void Rotate()
+    {
+        var rot = new Vector3(0f, 0f, 0f);
+        // rotates Camera Left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rot.y -= 1;
+        }
+        // rotates Camera Left
+        if (Input.GetKey(KeyCode.E))
+        {
+            rot.y += 1;
+        }
+
+
+        transform.Rotate(rot, rotate_speed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        Rotate();
+        transform.Translate(move_speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, move_speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
