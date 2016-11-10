8ab1be5a2a9c2c4e2c4310fdefa8981da0f88d6c
ec965 <enoch965@gmail.com>
2016-11-09 16:50:34 -0800

    Timer and ui placeholders
    Timer in the right top corner counts down from 10 mins.
    
    white images as placeholders for the cards and the map.

---
+++ Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    public Text timer_text;
+    private float start_time;
+	// Use this for initialization
+	void Start () {
+        start_time = Time.time;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time - 600;
+
+        string minutes =(-((int)t / 60)).ToString();
+        string seconds = (-(t % 60)).ToString("f0");
+
+        timer_text.text = minutes + ":" + seconds;
+        // add code to stop timer when you reach the end and win
+	
+	}
+}
