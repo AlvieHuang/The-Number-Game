8262d442527628346b678125d78accd67c50dab5
jersonv <jersonv@uci.edu>
2016-11-21 12:24:35 -0800

    Made a CardSpawner that spawns rectangle objects for now

---
+++ Assets/CardSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardSpawner : MonoBehaviour
+{
+    private GameObject[] spawnPoints;
+    private bool Run
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    void Awake()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("CardSpawn"); //Finds the spawnpoints for the cards
+        Spawn(spawnPoints, Resources.Load("Prefabs/Card") as GameObject); //spawn Cards
+    }
+
+    void Spawn(GameObject[] spawnPoints, GameObject prefab)
+    {
+        for (int i = 0; i < spawnPoints.Length; ++i)
+        {
+                GameObject go = Instantiate(prefab);
+                go.transform.position = spawnPoints[i].transform.position;
+                spawnPoints[i] = null;
+        }
+
+    }
+}
