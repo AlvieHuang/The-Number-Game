d9d7c5af911b089365cbdf5b003d639f44d931c1
SurajSSingh <Main@dhcp-v071-189.mobile.uci.edu>
2016-11-23 17:58:04 -0800

    GUI Tweaks and Mouse Update
    Changed the color of the timer.
    Moved inventory position to stick to bottom left corner.
     Mouse now displays debug on what is selected (eventually used for
    clicking).

--- Assets/Utility/MouseClick.cs
+++ Assets/Utility/MouseClick.cs
@@ -10,6 +10,55 @@ public class MouseClick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		//Creates the ray for the coursor
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast (ray, out hitInfo)) {
+			//Get the main object that the ray is in contact with
+			Debug.Log ("Mouse is Over: " + hitInfo.collider.name);
+			GameObject hitObject = hitInfo.collider.gameObject;
+
+			selectObject (hitObject);
+		} 
+		else {
+			ClearSelection ();
+		}
+
+	}
+	void selectObject(GameObject obj){
+		GameObject selectObject = obj;
+		if (selectObject != null) {
+			if (obj == selectObject) 
+				return;
+
+			ClearSelection ();
+		}
+
+		selectObject = obj;
+
+		Renderer[] render = selectObject.GetComponentsInChildren<Renderer>();
+		foreach (Renderer r in render) {
+			Material mat = r.material;
+			mat.color = Color.green;
+			r.material = mat;
+		}
+	}
+
+	void ClearSelection(){
+		GameObject selectObject = null;
+		if (selectObject == null) {
+			return;
+		}
+
+		Renderer[] render = selectObject.GetComponentsInChildren<Renderer>();
+		foreach (Renderer r in render) {
+			Material mat = r.material;
+			mat.color = Color.blue;
+			r.material = mat;
+
+
+			selectObject = null;
+		}
 	}
 }
